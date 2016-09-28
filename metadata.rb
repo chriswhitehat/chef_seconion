187d5fea58811328994d0d974245968c73f308e1
Chris White <white.chris@ghc.org>
2016-09-28 12:20:47 -0700

    b

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chris.white.hat@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures Security Onion'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.2.34'
+version          '0.2.35'
 provides         'seconion::server'
 provides         'seconion::sensor'
 
