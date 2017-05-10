16f7c3c9e7e00ca732e50ab8a606e49e5bc1df53
White <white.chris@ghc.org>
2017-05-09 20:52:40 -0700

    mysql tuning debug

--- recipes/server.rb
+++ recipes/server.rb
@@ -147,6 +147,7 @@ ruby_block "set_mysql_tuning_variables" do
     tuned_total = 0
     recommendations.each do |line|
       line_match = line.match(/\s+(?<variable>\w+)\s\(\>\=?\s(?<value>[0-9\.]+)(?<unit>\w)?/)
+      puts line_match
       if line_match
         if line_match[:unit]
           if line_match[:unit] == 'K'
