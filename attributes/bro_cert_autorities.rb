9e808d478cda705ed9e4931ed03ef108eed85f8f
Chris White <white.chris@ghc.org>
2017-07-05 14:55:56 -0700

    modified default cert_authorities attributes file to warn of double backslash requirements.  Added default false setting to new cert_authorities script

--- attributes/bro_cert_autorities.rb
+++ attributes/bro_cert_autorities.rb
@@ -2,5 +2,8 @@
 # See the following gist as an option for generating the hex string using gen_certs.py:
 # https://gist.github.com/JustinAzoff/7a1b92c976a2fa6e8601
 
-# Example usage
-#default[:seconion][:sensor][:bro][:cert_authorities]["<name_of_CA>"] = "\x2D\x2D\x2D\x2D\x2D.....hex cert string" 
+# Example usage 
+#################### Remember ######################
+# The expected results is the \x2D escaped version be sure to escape the escapes, note the \\ below
+####################################################
+#default[:seconion][:sensor][:bro][:cert_authorities]["<name_of_CA>"] = "\\x2D\\x2D\\x2D\\x2D\\x2D.....hex cert string" 
