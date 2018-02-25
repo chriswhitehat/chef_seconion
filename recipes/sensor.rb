128e7795fa7f0a314b8afcc4c19c84778a4bab7f
Chris White <chriswhitehat@gmail.com>
2018-02-25 00:19:15 -0800

    enabling ossec

--- recipes/sensor.rb
+++ recipes/sensor.rb
@@ -252,7 +252,7 @@ if node[:seconion][:sensor][:ossec_enabled]
     group 'ossec'
     mode '0644'
     notifies :restart, 'service[ossec-hids-server]', :delayed
-    notifies :restart, 'execute[restart_ossec_agent]', :delayed
+    notifies :run, 'execute[restart_ossec_agent]', :delayed
   end
   
 
