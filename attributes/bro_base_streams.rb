fbfe76a836c3d230d3a03791f66304f257dee5f1
Chris White <white.chris@ghc.org>
2018-06-25 14:11:23 -0700

    erroneous default attrs

--- attributes/bro_base_streams.rb
+++
@@ -1,9 +0,0 @@
-#
-# Cookbook Name:: seconion
-# Attribute:: bro_base_streams
-#
-
-# The following attributes control base streams in Bro. E.g. Disable Syslog
-
-# Disable Syslog Stream
-default[:seconion][:sensor][:bro_base_streams][:global]['base/protocols/syslog'] = false
