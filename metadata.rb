a212962591a0ff8ef8f2bbcdb8ed1a89e9568651
Chris White <chriswhite@cmwftw.local>
2019-05-30 01:40:21 -0700

    rule-update has a cron sleep of 60m defeating the purpose, moved @reboot to seconion-soup

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'chris.white.hat@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures/Maintains Security Onion'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.29.0'
+version          '0.29.1'
 
 provides         'seconion::server'
 provides         'seconion::sensor'
