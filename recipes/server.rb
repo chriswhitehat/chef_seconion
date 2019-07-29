f52e186d60b05d9cab00b5c0ceecd11841bd6bd7
Chris White <chriswhite@cmwftw.ghc.org>
2019-07-29 15:41:37 -0700

    added fix for set auth strategy missing creates

--- recipes/server.rb
+++ recipes/server.rb
@@ -188,12 +188,10 @@ template '/etc/mysql/conf.d/securityonion-ibdata1.cnf' do
 end
 
 execute 'set_root_auth_strategy' do
-  command "mysql -u root -D #{node[:seconion][:server][:sguil_server_name]}_db -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';\""
+  command "mysql -u root -D #{node[:seconion][:server][:sguil_server_name]}_db -e \"ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY '';\" && touch /root/mysql_native_#{ node[:seconion][:server][:sguil_server_name] }"
   action :run
-  not_if do ::File.exists?("/root/mysql_native_#{ node[:seconion][:server][:sguil_server_name] }") end
+  creates "/root/mysql_native_#{ node[:seconion][:server][:sguil_server_name] }"
 end
-
-
 
 # Only optimize on the first Monday of the month at noon local to the physical location of the server
 chef_runtime = Time.now.utc.localtime(node[:seconion][:physical_timezone_offset])
