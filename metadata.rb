98821354dfad6367d98628b79d2916b6ca2f7c7d
Dan Casey <dcasey@scnchf01094p01.sec.costco.com>
2020-02-19 11:31:33 -0800

    changing daystokeep back to 30

--- metadata.rb
+++ metadata.rb
@@ -5,7 +5,7 @@ license          'All rights reserved'
 description      'Installs/Configures/Maintains Security Onion'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 
-version          '0.42.2'
+version          '0.42.3'
 
 provides         'seconion::server'
 provides         'seconion::sensor'
